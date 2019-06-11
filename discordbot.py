7ad960e3d8d0d90dd2223c49451eeb5b2a4a8118
Terurunn1 <yosuke.teru82@gmail.com>
2019-06-11 22:05:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -28,7 +28,7 @@ async def greeting_gm():
             outPut +="\n怒りのバフォメット : キャッスルリバース"
             outPut +="\nアクレオン : ラベンダー島"
             outPut +="\nイカル・マード : アイスクラウン"
-        elif 10 >= nowMinutes :
+        elif 10 == nowMinutes :
             outPut +="以下のボスが次回(15分)出現します。"
             outPut +="\nグリフォン : 冷たい心臓"
             outPut +="\nウレウス : ナズカル入口"

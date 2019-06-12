d286d1ba88a2fe9c0278a0120bb82b1c5b7f7ce8
Terurunn1 <yosuke.teru82@gmail.com>
2019-06-12 09:06:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from datetime import datetime
 import os
 import traceback
+import asyncio
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']

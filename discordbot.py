bb0966f126f6c5fa974fdd3ccab427517ecd95ee
Terurunn1 <yosuke.teru82@gmail.com>
2019-06-12 10:01:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,23 @@
 import discord
 import asyncio
+import traceback
+from discord.ext import commands
+from datetime import datetime
 
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
 
 @client.event
 async def on_ready():
     asyncio.ensure_future(greeting_gm())
 
 async def greeting_gm():
-    await client.send_message(channel, 'おはよう')
+    await client.send_message(general, 'おはよう')
     await asyncio.sleep(10)
 
 client.run(token)

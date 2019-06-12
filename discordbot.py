cc747962ccf346862e43a8745df2d49db8b2157b
Terurunn1 <yosuke.teru82@gmail.com>
2019-06-12 10:02:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_ready():
     asyncio.ensure_future(greeting_gm())
 
 async def greeting_gm():
-    await client.send_message(general, 'おはよう')
+    await client.send_message(channel, 'おはよう')
     await asyncio.sleep(10)
 
 client.run(token)

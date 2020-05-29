2325c2cc82738353ef960cbbedb82df3b7486297
Terurunn1 <youme.hawking@gmail.com>
2020-05-29 09:54:22 +0900

    Update discordbot.py
    Signed-off-by: Terurunn1 <youme.hawking@gmail.com>

--- discordbot.py
+++ discordbot.py
@@ -15,17 +15,22 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def tsc(ctx):
-    """BOTOの説明用コマンド"""
+    """BOTの説明用コマンド"""
+    viewrecipes();
     await ctx.send('使いたい機能を実行する際は以下のコマンドを入力してください\n ■ブルプロクラフトレシピ /bpre [item No]\n ■ブルプロクラフト品リスト /bpli')
 
 @bot.command()
 async def bpre(ctx, arg):
     """該当アイテムのレシピを表示します。"""
-    await ctx.send(arg+'番のアイテムが選択されました。(テスト)')
+    await ctx.send(arg+'の作成には以下の素材が必要です。\nトリックエルダーの爪：１個\n湧水石：50個')
                    
 @bot.command()
 async def bpli(ctx):
     """ブルプロのクラフト対象アイテムリストを表示します。"""
     await ctx.send('動作テスト')
-                   
+                 
+    
+def viewrecipes():
+    print('Hello')
+
 bot.run(token)
